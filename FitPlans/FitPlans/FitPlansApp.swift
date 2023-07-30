505c27fdfedb303c29766f7cc1860843c73b2da5
Zdenko Čepan <zdenkocepan@MacStudlaZdenko.docsis.vodafone.cz>
2023-07-30 14:23:08 +0200

    create project

---
+++ FitPlans/FitPlans/FitPlansApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitPlansApp.swift
+//  FitPlans
+//
+//  Created by Zdenko Čepan on 30.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct FitPlansApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
